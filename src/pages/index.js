b2bb0703b672c2af8bc4d71d0fd910f18de5aaa2
Hmorgan22 <huntermorgan1219@yahoo.com>
2023-09-21 12:09:44 -0400

    Added states and links to state sites.

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,13 +16,6 @@ const headingAccentStyles = {
 const paragraphStyles = {
   marginBottom: 48,
 }
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
 const listStyles = {
   marginBottom: 96,
   paddingLeft: 0,
@@ -41,11 +34,6 @@ const linkStyle = {
   verticalAlign: "5%",
 }
 
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
-}
 
 const descriptionStyle = {
   color: "#232129",
@@ -55,70 +43,48 @@ const descriptionStyle = {
   lineHeight: 1.25,
 }
 
-const docLink = {
-  text: "Documentation",
-  url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
-}
-
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
-
 const links = [
   {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/getting-started/",
+    text: "Hawaii",
+    url: "https://naturallyaloha.com/planning-a-hawaii-vacation-the-ultimate-guide/",
     description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
+      "This is a great place to go vacation if you are in the mood for some sun and nice warm weather!",
     color: "#E95800",
   },
   {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
+    text: "New York",
+    url: "https://travel.usnews.com/rankings/best-places-to-visit-in-new-york/",
     description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
+      "This trip is great for anyone that wants to go vist a big city or try some legendary food!",
     color: "#1099A8",
   },
   {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
+    text: "Colorado",
+    url: "https://travel.usnews.com/rankings/best-places-to-visit-in-colorado/",
     description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
+      "This trip is amazing for anyone that wants to be outdoors and enjoys hiking and climbing or sking and snowboarding depending what time of the year you go!",
     color: "#BC027F",
   },
   {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
+    text: "Alaska",
+    url: "https://www.alaska.org/trip-ideas",
     description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
+      "This destination is great for anyone that wants to be outdoors but perfers to go fishing and hunting!",
     color: "#0D96F2",
   },
   {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
+    text: "Florida",
+    url: "https://www.travelandleisure.com/trip-ideas/beach-vacations/best-places-to-visit-in-florida",
     description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
+      "This is another great beach vacation trip for anyone that is wanting to spend some time in the sun and out by the sea!",
     color: "#8EB814",
   },
   {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
+    text: "Texas",
+    url: "https://travel.usnews.com/rankings/best-places-to-visit-in-texas/",
     badge: true,
     description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
+      "You can never go wrong with wanting to go on a trip to the largest state in the U.S. This trip is great for anyone that wants to experience the vast landscapes of the south!",
     color: "#663399",
   },
 ]
@@ -127,23 +93,13 @@ const IndexPage = () => {
   return (
     <main style={pageStyles}>
       <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— testing. This is a test site 🎉🎉🎉</span>
+        <span style={headingAccentStyles}>Welcome to Vacation Ideas!</span>
       </h1>
       <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time. 😎
+        This site helps you see images of different popular vaction locations in the United States. That way you can remove the pressure of finding the perfect vacation spot and get going!
+        Click on the state name to see some popular vacation ideas for that state.
       </p>
       <ul style={listStyles}>
-        <li style={docLinkStyle}>
-          <a
-            style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-          >
-            {docLink.text}
-          </a>
-        </li>
         {links.map(link => (
           <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
             <span>
@@ -153,24 +109,15 @@ const IndexPage = () => {
               >
                 {link.text}
               </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
               <p style={descriptionStyle}>{link.description}</p>
             </span>
           </li>
         ))}
       </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
     </main>
   )
 }
 
 export default IndexPage
 
-export const Head = () => <title>Home Page</title>
+export const Head = () => <title>Vacation Ideas</title>
